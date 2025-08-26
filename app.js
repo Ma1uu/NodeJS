6f55a798df1aa9f562e7b16f667b8c42e06742d6
Maria Luiza <maria.silva5315@etec.sp.gov.br>
2025-08-26 00:15:27 -0300

    consertando

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 // Configurações
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'nav-node.js', 'views'));
+console.log("Views dir:", path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
