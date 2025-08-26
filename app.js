0775352340f181e6e2796fb12ad4ef6d6153d41b
Maria Luiza <maria.silva5315@etec.sp.gov.br>
2025-08-26 10:47:02 -0300

    finalizando

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Dados de exemplo
-const categorias = [
-  { id: 1, nome: 'Programação' },
-  { id: 2, nome: 'Design' }
-];
 
 const alunos = [
   { id: 1, nome: 'Maria' },
@@ -24,15 +20,8 @@ const cursos = [
   { id: 2, nome: 'Bootstrap Avançado' }
 ];
 
-const equipe = [
-  { id: 1, nome: 'Malu Bonato', funcao: 'Desenvolvedora' },
-  { id: 2, nome: 'João Silva', funcao: 'Designer' }
-];
-
 // Rotas
 app.get('/', (req, res) => res.render('index'));
-
-app.get('/categorias', (req, res) => res.render('categorias/listar', { categorias }));
 app.get('/alunos', (req, res) => res.render('alunos/listar', { alunos }));
 app.get('/cursos', (req, res) => res.render('cursos/listar', { cursos }));
 app.get('/equipe', (req, res) => res.render('equipe', { equipe }));
